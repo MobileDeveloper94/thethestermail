63267b8ed52cb84c39ce0f645e124d7556cae6b9
Paulo Sergio Duarte Lima <paulo.lima@suppliercard.com.br>
2021-02-24 18:16:47 -0300

    resolvendo cacas

--- email.php
+++ email.php
@@ -1,23 +1,53 @@
-<!Doctype Html5>
-<html>
-<head>
-<title>Teste de envio de e-mail</title>
-</head>
-<body>
-<form action="email.php" method="post">
-	<label for="Nome">Nome:</label>
-	<input type="text" name="Nome" size="35" />
+<?php
+$Nome			= $_POST["Nome"];	// Pega o valor do campo Nome
+$EmailFrom		= $_POST["EmailFrom"]; // Pega o valor do campo Email do cliente
+$EmailTo		= $_POST["EmailTo"];	// Pega o valor do campo Email que vai receber
+$Mensagem		= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
+$Assunto 		= $_POST["Assunto"];	//assunto do email
+$Alias			= $_POST["Alias"];		//alias do email, ex: The Thester
 
-	<label for="EmailFrom">E-mail From:</label>
-	<input type="text" name="EmailFrom" size="35" />
-	
-	<label for="EmailTo">E-mail To:</label>
-	<input type="text" name="EmailFrom" size="35" />
+// Variável que junta os valores acima e monta o corpo do email
 
-	<label for="Mensagem">Mensagem:</label>
-	<textarea name="Mensagem" rows="8" cols="40"></textarea>
+$Vai 		= "Nome: $Nome\n\nE-mail: $EmailFrom\n\nMensagem: $Mensagem\n";
 
-	<input type="submit" name="Enviar" value="Enviar" />
-</form>
-</body>
-</html>
+require_once("phpmailer/class.phpmailer.php");
+
+define('GUSER', 'paulosduarte7@gmail.com');	// <-- Insira aqui o seu GMail
+define('GPWD', 'P@ralelepiped0');		// <-- Insira aqui a senha do seu GMail
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+	global $error;
+	$mail = new PHPMailer();
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = GUSER;
+	$mail->Password = GPWD;
+	$mail->SetFrom($de, $de_nome);
+	$mail->Subject = $assunto;
+	$mail->Body = $corpo;
+	$mail->AddAddress($para);
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+	} else {
+		$error = 'Mensagem enviada!';
+		return true;
+	}
+}
+
+// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
+//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+
+smtpmailer($EmailTo, GUSER, $Alias, $Assunto, $Vai)) 
+
+header('Content-type: application/json'); 
+
+$res = [ 'retorno' => $errror];
+
+echo json_encode($res);
+
+?>
--- teste.php
+++ teste.php
@@ -1,51 +1,34 @@
-<?php
-$Nome		= $_POST["Nome"];	// Pega o valor do campo Nome
-$EmailFrom		= $_POST["EmailFrom"]; // Pega o valor do campo Email do cliente
-$EmailTo		= $_POST["EmailTo"];	// Pega o valor do campo Email que vai receber
-$Mensagem	= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
+<!Doctype Html5>
+<html>
+<head>
+<title>Teste de envio de e-mail</title>
+</head>
+<body>
+<form action="email.php" method="post">
+	
+	<h3>Infos remetente</h3>
+	<hr>
+	<label for="EmailFrom">E-mail From:</label>
+	<input type="text" name="EmailFrom" size="35" />
 
-// Variável que junta os valores acima e monta o corpo do email
+	<label for="Alias">Alias:</label>
+	<input type="text" name="Alias" size="35" />
+	
+	<label for="Assunto">Assunto:</label>
+	<input type="text" name="Assunto" size="35" />
+	
+	<h3>Infos destinatário</h3>
+	<hr>
+	<label for="EmailTo">E-mail To:</label>
+	<input type="text" name="EmailFrom" size="35" />
 
-$Vai 		= "Nome: $Nome\n\nE-mail: $EmailFrom\n\nMensagem: $Mensagem\n";
+	<label for="Nome">Nome:</label>
+	<input type="text" name="Nome" size="35" />
 
-require_once("phpmailer/class.phpmailer.php");
+	<label for="Mensagem">Mensagem:</label>
+	<textarea name="Mensagem" rows="8" cols="40"></textarea>
 
-define('GUSER', 'paulosduarte7@gmail.com');	// <-- Insira aqui o seu GMail
-define('GPWD', 'P@ralelepiped0');		// <-- Insira aqui a senha do seu GMail
-
-function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
-	global $error;
-	$mail = new PHPMailer();
-	$mail->IsSMTP();		// Ativar SMTP
-	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
-	$mail->SMTPAuth = true;		// Autenticação ativada
-	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
-	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
-	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
-	$mail->Username = GUSER;
-	$mail->Password = GPWD;
-	$mail->SetFrom($de, $de_nome);
-	$mail->Subject = $assunto;
-	$mail->Body = $corpo;
-	$mail->AddAddress($para);
-	if(!$mail->Send()) {
-		$error = 'Mail error: '.$mail->ErrorInfo; 
-		return false;
-	} else {
-		$error = 'Mensagem enviada!';
-		return true;
-	}
-}
-
-// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
-//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
-
- smtpmailer($EmailTo, GUSER, 'The Thester Site', $Nome . ' enviou uma mensagem', $Vai)) 
-
-header('Content-type: application/json'); 
-
-$res = [ 'retorno' => $errror];
-
-echo json_encode($res);
-
-?>
+	<input type="submit" name="Enviar" value="Enviar" />
+</form>
+</body>
+</html>
