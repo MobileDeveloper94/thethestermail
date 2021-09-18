14e72abd81f082ea5cafc74cd5a568021e6aed10
Paulo Sergio Duarte Lima <paulo.lima@suppliercard.com.br>
2021-09-18 08:43:40 -0300

    api de autenticacao

--- autenticacao.php
+++ autenticacao.php
@@ -1,8 +1,8 @@
 <?php
 
 header('Content-type: application/json');
-header('Access-Control-Allow-Origin: https://hmlthethester.netlify.app');
-header('Access-Control-Allow-Origin: https://thethester.com.br');
+// header('Access-Control-Allow-Origin: https://hmlthethester.netlify.app');
+// header('Access-Control-Allow-Origin: https://thethester.com.br');
 header("Access-Control-Allow-Origin: *");//test only
 header('Access-Control-Allow-Methods: POST, GET');
 header('Access-Control-Allow-Headers: *');
@@ -43,44 +43,151 @@ function Login($obj){
 	try {
 		$pdo = OpenDB();
 		
-		
-        
 		if($pdo){
 			
-			$stmt = $pdo->prepare('SELECT * FROM LOGIN WHERE login = :login AND senha = :senha');
+			$stmt = $pdo->prepare('SELECT * FROM LOGIN WHERE email = :email AND senha = :senha');
 			$stmt->execute(array(
-			':login' => $obj->login, 
+			':email' => $obj->email, 
             ':senha' => sha1($obj->senha)
 			));
 			
 			if($row = $stmt->fetch()){
-                session_start();
-
-                $_SESSION['id'] =	$row['id'];
-                $_SESSION['id'] =	$row['login'];
-                $_SESSION['id'] =	$row['senha'];
-                $_SESSION['id'] =	$row['cod_assoc'];
-                $_SESSION['id'] =	$row['dta_inscricao'];
-                $_SESSION['id'] =	$row['dta_vencimento'];
-                $_SESSION['id'] =	$row['fl_ativo'];
-                $_SESSION['id'] =	$row['id_tipo_acesso'];
-
-            }
+				
+				if(isset($_SESSION)){
+					session_destroy();
+				}
+					
+				session_start();
+				$_SESSION['id'] = session_id();
+				$_SESSION['id_login'] =	$row['id'];
+				$_SESSION['token_autenticacao'] = sha1("snorlax" . gmdate('Y-m-d h:i:s \G\M\T'));
+				
+            }else{
+				echo json_encode(['sucesso' => false , 'mensagem' => 'Usuário ou senha inválidos.']);
+				die();
+			}
 			
-            $stmt = $pdo->prepare("INSERT INTO AUTENTICACAO_LOG (dt_Alteracao, id_login, observacao) VALUES (NOW(), :id_login, 'Login de usuário efetuado'");
+            $stmt = $pdo->prepare("INSERT INTO LOGIN_LOG (dt_Alteracao, id_login, observacao) VALUES (NOW(), :id_login, :mensagem)");
 			$stmt->execute(array(
-			':id_login' => $_SESSION['id']
+			':id_login' => $_SESSION['id_login'],
+			':mensagem' => "ACESSO / " . $obj->useragent
 			));
 
-			echo json_encode(['sucesso' => true , 'mensagem' => 'Usuário autenticado com sucesso.', 'id' => $row['id']]);
+			$stmt = $pdo->prepare("DELETE FROM AUTENTICACAO_AUX WHERE id_login = :id_login;");
+			$stmt->execute(array(
+			':id_login' => $_SESSION['id_login']
+			));
+
+			$stmt = $pdo->prepare("INSERT INTO AUTENTICACAO_AUX (id_login, dta_expiracao, token) VALUES (:id_login, date_add(now(), interval 15 MINUTE), :token);");
+			$stmt->execute(array(
+			':id_login' => $_SESSION['id_login'],
+			':token' => $_SESSION['token_autenticacao']
+			));
+
+			echo json_encode(['sucesso' => true , 'mensagem' => 'Usuário autenticado com sucesso.', 'id' => $_SESSION['id_login'], 'token' => $_SESSION['token_autenticacao']]);
 		}
 
 	}catch(Exception $e){
-		echo json_encode(['sucesso' => false , 'mensagem' => $e->GetMessage()]);
-		
+		echo json_encode(['sucesso' => false , 'mensagem' => $e->GetMessage()]);		
 	}	
 }
 
+function Logout($obj){
+	try {
+		$pdo = OpenDB();
+		
+		if($pdo){
+			$stmt = $pdo->prepare("DELETE FROM AUTENTICACAO_AUX WHERE token = :token");
+			$stmt->execute(array(
+			':token' => $obj->token
+			));
+				
+			if(isset($_SESSION)){
+				session_destroy();
+			}
+
+			echo json_encode(['sucesso' => true , 'mensagem' => 'Logout efetuado com sucesso.']);
+		}
+	}catch(Exception $e){
+		echo json_encode(['sucesso' => false , 'mensagem' => $e->GetMessage()]);
+	}	
+}
+
+function Check($obj){
+	try {
+		$pdo = OpenDB();
+		
+		if($pdo){
+			
+			if(isset($_SESSION)){
+				
+				if($_SESSION['token_autenticacao'] = $obj->token){
+					
+					$stmt = $pdo->prepare("SELECT * FROM AUTENTICACAO_AUX WHERE id_login = :id_login and token = :token AND NOW() < dta_expiracao");
+					
+					$stmt->execute(array(
+					':id_login' => $_SESSION['id_login'],
+					':token' => $_SESSION['token_autenticacao']
+					));
+					
+					if($row = $stmt->fetch()){
+						
+						$stmt = $pdo->prepare("DELETE FROM AUTENTICACAO_AUX WHERE id_login = :id_login;");
+						$stmt->execute(array(
+						':id_login' => $_SESSION['id_login']
+						));
+						
+						$_SESSION['token_autenticacao'] = sha1("snorlax" . gmdate('Y-m-d h:i:s \G\M\T'));
+
+						$stmt = $pdo->prepare("INSERT INTO AUTENTICACAO_AUX (id_login, dta_expiracao, token) VALUES (:id_login, date_add(now(), interval 15 MINUTE), :token);");
+						$stmt->execute(array(
+						':id_login' => $_SESSION['id_login'],
+						':token' => $_SESSION['token_autenticacao']
+						));		
+
+						echo json_encode(['sucesso' => true , 'mensagem' => 'Usuário autenticado com sucesso.', 'id' => $_SESSION['id_login'], 'token' => $_SESSION['token_autenticacao']]);
+					}else{
+						echo json_encode(['sucesso' => false , 'mensagem' => 'Usuário não autenticado']);
+						die();
+					}
+				}else{
+					echo json_encode(['sucesso' => false , 'mensagem' => 'Usuário não autenticado']);
+					die();
+				}
+			}else{
+				$stmt = $pdo->prepare("SELECT * FROM AUTENTICACAO_AUX WHERE token = :token AND NOW() < dta_expiracao");
+				$stmt->execute(array(
+				':token' => $obj->token
+				));
+
+				if($row = $stmt->fetch()){
+					session_start();
+					$_SESSION['id'] = session_id();
+					$_SESSION['id_login'] =	$row['id_login'];
+					$_SESSION['token_autenticacao'] = sha1("snorlax" . gmdate('Y-m-d h:i:s \G\M\T'));
+
+					$stmt = $pdo->prepare("DELETE FROM AUTENTICACAO_AUX WHERE id_login = :id_login;");
+					$stmt->execute(array(
+					':id_login' => $_SESSION['id_login']
+					));
+					
+					$stmt = $pdo->prepare("INSERT INTO AUTENTICACAO_AUX (id_login, dta_expiracao, token) VALUES (:id_login, date_add(now(), interval 15 MINUTE), :token);");
+					$stmt->execute(array(
+					':id_login' => $_SESSION['id_login'],
+					':token' => $_SESSION['token_autenticacao']
+					));
+					
+					echo json_encode(['sucesso' => true , 'mensagem' => 'Usuário autenticado com sucesso.', 'id' => $_SESSION['id_login'], 'token' => $_SESSION['token_autenticacao']]);
+				}else{
+					echo json_encode(['sucesso' => false , 'mensagem' => 'Usuário não autenticado']);
+					die();
+				}
+			}	
+		}
+	}catch(Exception $e){
+		echo json_encode(['sucesso' => false , 'mensagem' => $e->GetMessage()]);		
+	}
+}
 
 
 ?>
